9032e0a581beec9ed401f8ece948e693435ac832
Noda <r.noda@a-force.co.jp>
2022-09-29 17:54:43 +0900

    外部テンプレート移植（一部直埋め、要切り分け）

--- app/src/br-sample/app/Http/Controllers/ctl/BrPartnerCustomerController.php
+++ app/src/br-sample/app/Http/Controllers/ctl/BrPartnerCustomerController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\ctl;
 
 class BrPartnerCustomerController extends _commonController
 {
-    public function search() 
+    public function search()
     {
         // オブジェクトの取得
-        
+
         return view('ctl.brPartnerCustomer.search');
     }
 }
