37529a26af2e7bf70581256b535853ee0e0eda5e
Noda <r.noda@a-force.co.jp>
2022-10-21 10:49:22 +0900

    [update] edit: form_params, search_params, error

--- app/src/br-sample/app/Http/Controllers/ctl/BrPartnerSiteController.php
+++ app/src/br-sample/app/Http/Controllers/ctl/BrPartnerSiteController.php
@@ -54,6 +54,11 @@ class BrPartnerSiteController extends _commonController
      */
     public function edit(Request $request)
     {
+        $errors = [];
+
+        // MEMO: 検索条件の引き回し HACK: session で管理したい
+        $form_params = $request->input();
+
         // 精算先登録情報設定
         $model = new PartnerSite();
         if ($request->has('partner_site')) {
@@ -65,12 +70,29 @@ class BrPartnerSiteController extends _commonController
                 $partner_sites = $model->_get_sites(['site_cd' => $request->input('site_cd')]);
 
                 if (count($partner_sites) < 1) {
-                    // TODO: error
+                    // TODO: error (redirect to search?)
+                    // 現行に合わせるのであれば、すべてが空のフォームを表示（site_cd 必須バリデーションに引っかかるので、戻らざるを得ない）
+                    $errors[] = '対象となる精算サイトは見つかりませんでした。';
+                    $partner_site = (object)[
+                        'site_cd'       => '', //$model->_get_sequence_no(),
+                        'site_nm'       => '',
+                        'person_post'   => '',
+                        'person_nm'     => '',
+                        'email_decrypt' => '',
+                        'mail_send'     => 0,
+                        'partner_cd'    => '',
+                        'partner_nm'    => '',
+                        'affiliate_cd'  => '',
+                        'affiliate_nm'  => '',
+                        'sales_customer_id' => '',
+                        'sales_customer_nm' => '',
+                    ];
                 } else {
                     $partner_site = $partner_sites[0];
                 }
             } else {
                 // サイトコードが未指定の場合は、新規登録とする
+                $form_params['site_cd'] = null; // MEMO: HACK: 現行仕様に合わせているが未定義だと Warning が発生する。
                 // HACK: オブジェクトをうまく初期化する方法がありそう。
                 $partner_site = (object)[
                     'site_cd'       => $model->_get_sequence_no(),
@@ -83,6 +105,8 @@ class BrPartnerSiteController extends _commonController
                     'partner_nm'    => '',
                     'affiliate_cd'  => '',
                     'affiliate_nm'  => '',
+                    'sales_customer_id' => '',
+                    'sales_customer_nm' => '',
                 ];
             }
         }
@@ -90,12 +114,19 @@ class BrPartnerSiteController extends _commonController
         // 手数料率設定
         $rates = $model->_get_rates(['site_cd' => $partner_site->site_cd]);
 
-        // TODO:
-        $form_params = [];
-        $form_params['site_cd'] = $request->input('site_cd', '');
-
-        // TODO:
-        $search_params = [];
+        // 精算先情報設定
+        $partner_customer_site = [];
+        $partner_customer_site['customer_id'] = $partner_site->sales_customer_id;
+        $partner_customer_site['customer_nm'] = $partner_site->sales_customer_nm;
+        // サイトコードが未指定で精算先が指定されてきていて、精算先が確定してない場合は、指定されてきたIDを初期値として設定
+        if (
+                is_null($partner_customer_site['customer_id']) // TODO: 要確認（現行では is_empty() で判定）
+            && $request->has('customer_id')
+            && $request->input('customer_id') != 1
+            && !$request->has('site_cd')
+        ) {
+            $partner_customer_site['customer_id'] = $request->input('customer_id');
+        }
 
         // 料率設定
         $partner_site_rate = [];
@@ -109,25 +140,20 @@ class BrPartnerSiteController extends _commonController
             $partner_site_rate['accept_s_ymd']      = null;
         }
 
-        // 精算先情報設定
-        $partner_customer_site = [];
-        $partner_customer_site['customer_id'] = $partner_site->sales_customer_id;
-        $partner_customer_site['customer_nm'] = $partner_site->sales_customer_nm;
-        // サイトコードが未指定で精算先が指定されてきていて、精算先が確定してない場合は、指定されてきたIDを初期値として設定
-        if (
-                is_null($partner_customer_site['customer_id'])
-            and $request->has('customer_id')
-            and $request->input('customer_id') != 1
-            and !$request->has('site_cd')
-        ) {
-            $partner_customer_site['customer_id'] = $request->input('customer_id');
+        // MEMO: 検索条件の引き回し HACK: session で管理したい
+        $search_params = [];
+        if ($request->has('customer_id')) {
+            $search_params['customer_id'] = $request->input('customer_id');
+        }
+        if ($request->has('customer_off')) {
+            $search_params['customer_off'] = $request->input('customer_off');
+        }
+        if ($request->has('keywords')) {
+            $search_params['keywords'] = $request->input('keywords');
         }
 
-
         return view('ctl.brPartnerSite.edit', [
-            'errors' => [
-                'dummy error',
-            ],
+            'errors'                => $errors,
             'form_params'           => $form_params,
             'search_params'         => $search_params,
             'partner_site'          => $partner_site,
