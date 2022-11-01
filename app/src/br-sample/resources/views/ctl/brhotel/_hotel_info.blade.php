dd7a9bfe77859579e3d063164db064be05f2717f
Noda <r.noda@a-force.co.jp>
2022-11-01 12:02:39 +0900

    [update] ホテル情報ビュー indent

--- app/src/br-sample/resources/views/ctl/brhotel/_hotel_info.blade.php
+++ app/src/br-sample/resources/views/ctl/brhotel/_hotel_info.blade.php
@@ -1,24 +1,24 @@
 施設情報詳細
-    <table border="2" cellspacing="0" cellpadding="4">
-      <tr>
+<table border="2" cellspacing="0" cellpadding="4">
+    <tr>
         <td bgcolor="#EEFFEE">
-        施設コード
+            施設コード
         </td>
         <td>
-          {{strip_tags($hotel['hotel_cd'])}}<br />
+            {{ strip_tags($hotel['hotel_cd']) }}<br />
         </td>
-      </tr>
-      <tr>
+    </tr>
+    <tr>
         <td bgcolor="#EEFFEE">
-        施設名
+            施設名
         </td>
         <td>
-          {{strip_tags($hotel['hotel_nm'])}}<br />
+            {{ strip_tags($hotel['hotel_nm']) }}<br />
         </td>
-      </tr>
-      <tr>
+    </tr>
+    <tr>
         <td bgcolor="#EEFFEE">
-        都道府県
+            都道府県
         </td>
         <td>
             {{-- MEMO: 移植元ソースでは sprit_tags() を通しているが、マスタデータの表示なので必要ないと判断した。 --}}
@@ -26,5 +26,5 @@
             {{ !is_null($mast_city) ? $mast_city['city_nm'] : '' }}
             {{ !is_null($mast_ward) ? $mast_ward['ward_nm'] : '' }}
         </td>
-      </tr>
-    </table>
+    </tr>
+</table>
