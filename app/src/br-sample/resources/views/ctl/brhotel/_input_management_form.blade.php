6540ad4b96d6253417af8c6aba47476b53056c58
Noda <r.noda@a-force.co.jp>
2022-11-02 19:05:06 +0900

    [update] 施設管理情報更新画面 view, Form Facades に置き換え

--- app/src/br-sample/resources/views/ctl/brhotel/_input_management_form.blade.php
+++ app/src/br-sample/resources/views/ctl/brhotel/_input_management_form.blade.php
@@ -13,7 +13,7 @@
     <tr>
         <td bgcolor="#EEFFEE">アカウントID※</td>
         <td>
-            <input type="text" name="Hotel_Account[account_id_begin]" value="{{ strip_tags($hotel_account->account_id_begin) }}" size="15" maxlength="10">
+            {{ Form::text('Hotel_Account[account_id_begin]', strip_tags($hotel_account->account_id_begin), ['size' => '15', 'maxlength' => '10',]) }}
         </td>
         <td><small>10文字<font color="#0000ff">（必須）</font></small></td>
     </tr>
@@ -22,7 +22,7 @@
         <td bgcolor="#EEFFEE">パスワード※</td>
         <td>
             @if ($service->is_empty($disp))
-                <input type="text" name="Hotel_Account[password]" value="{{ strip_tags($hotel_account->password) }}" size="15" maxlength="10">
+                {{ Form::text('Hotel_Account[password]', strip_tags($hotel_account->password), ['size' => '15', 'maxlength' => '10',]) }}
             @else
                 <span nowrap style="float: left;">**********</span>
                 <span nowrap style="float: right;">
@@ -40,14 +40,10 @@
     <tr>
         <td bgcolor="#EEFFEE">ステータス</td>
         <td>
-            <input type="radio" name="Hotel_Account[accept_status]" value="1" id="i2" {{ $hotel_account->accept_status == 1 || $service->is_empty($hotel_account->accept_status) ? 'checked' : '' }}>
-            <label for="i2">
-                利用可
-            </label>
-            <input type="radio" name="Hotel_Account[accept_status]" value="0" id="i1" {{ $hotel_account->accept_status == 0 && !$service->is_empty($hotel_account->accept_status) ? 'checked' : '' }}>
-            <label for="i1">
-                利用不可
-            </label>
+            {{ Form::radio('Hotel_Account[accept_status]', '1', $hotel_account->accept_status == 1 || $service->is_empty($hotel_account->accept_status), ['id' => 'i2',]) }}
+            {{ Form::label('i2', '利用可') }}
+            {{ Form::radio('Hotel_Account[accept_status]', '0', $hotel_account->accept_status == 0 && !$service->is_empty($hotel_account->accept_status), ['id' => 'i1',]) }}
+            {{ Form::label('i1', '利用不可') }}
         </td>
         <td><small>選択</small></td>
     </tr>
@@ -55,7 +51,7 @@
     <tr>
         <td bgcolor="#EEFFEE">担当者役職</td>
         <td>
-            <input type="text" name="Hotel_Person[person_post]" value="{{ strip_tags($hotel_person->person_post) }}" size="50" maxlength="32">
+            {{ Form::text('Hotel_Person[person_post]', strip_tags($hotel_person->person_post), ['size' => '50', 'maxlength' => '32',]) }}
         </td>
         <td><small>32文字</small></td>
     </tr>
@@ -63,7 +59,7 @@
     <tr>
         <td bgcolor="#EEFFEE">担当者名称※</td>
         <td>
-            <input type="text" name="Hotel_Person[person_nm]" value="{{ strip_tags($hotel_person->person_nm) }}" size="50" maxlength="32">
+            {{ Form::text('Hotel_Person[person_nm]', strip_tags($hotel_person->person_nm), ['size' => '50', 'maxlength' => '32',]) }}
         </td>
         <td><small>32文字<font color="#0000ff">（必須）</font></small></td>
     </tr>
@@ -71,7 +67,7 @@
     <tr>
         <td bgcolor="#EEFFEE">担当者電話番号※</td>
         <td>
-            <input type="text" name="Hotel_Person[person_tel]" value="{{ $status == 'new' && $service->is_empty(strip_tags($hotel_person->person_tel)) ? strip_tags($hotel->tel) : strip_tags($hotel_person->person_tel) }}" size="20" maxlength="15">
+            {{ Form::text('Hotel_Person[person_tel]', ($status == 'new' && $service->is_empty(strip_tags($hotel_person->person_tel))) ? strip_tags($hotel->tel) : strip_tags($hotel_person->person_tel), ['size' => '20', 'maxlength' => '15',]) }}
         </td>
         <td><small>xxxx-xxxx-xxxx<font color="#0000ff">（必須）</font></small></td>
     </tr>
@@ -79,7 +75,7 @@
     <tr>
         <td bgcolor="#EEFFEE">担当者ファックス番号</td>
         <td>
-            <input type="text" name="Hotel_Person[person_fax]" value="{{ $status == 'new' && $service->is_empty(strip_tags($hotel_person->person_fax)) ? strip_tags($hotel->fax) : strip_tags($hotel_person->person_fax) }}" size="20" maxlength="15">
+            {{ Form::text('Hotel_Person[person_fax]', ($status == 'new' && $service->is_empty(strip_tags($hotel_person->person_fax))) ? strip_tags($hotel->fax) : strip_tags($hotel_person->person_fax), ['size' => '20', 'maxlength' => '15',]) }}
         </td>
         <td><small>xxxx-xxxx-xxxx</small></td>
     </tr>
@@ -87,7 +83,7 @@
     <tr>
         <td bgcolor="#EEFFEE">担当者電子メールアドレス</td>
         <td>
-            <input type="text" name="Hotel_Person[person_email]" value="{{ strip_tags($hotel_person->person_email) }}" size="50" maxlength="50">
+            {{ Form::text('Hotel_Person[person_email]', strip_tags($hotel_person->person_email), ['size' => '50', 'maxlength' => '50',]) }}
         </td>
         <td><br /></td>
     </tr>
@@ -97,19 +93,19 @@
         <td bgcolor="#EEFFEE">登録状態</td>
         @if ($new_flg == 1)
             <td>
-                <input type="hidden" name="Hotel_Status[entry_status]" value="1" />
+                {{ Form::hidden('Hotel_Status[entry_status]', 1) }}
                 登録作業中
             </td>
         @else
             <td>
-                <input type="radio" name="Hotel_Status[entry_status]" value="0" id="k1" {{ $hotel_status->entry_status == 0 ? 'checked' : '' }} {{ !$rate_chk ? 'disabled' : '' }}>
-                <label for="k1">公開中</label>
+                {{ Form::radio('Hotel_Status[entry_status]', '0', $hotel_status->entry_status == 0, ['id' => 'k1', 'disabled' => !$rate_chk,]) }}
+                {{ Form::label('k1', '公開中') }}
 
-                <input type="radio" name="Hotel_Status[entry_status]" value="1" id="k2" {{ $hotel_status->entry_status == 1 ? 'checked' : '' }} >
-                <label for="k2">登録作業中</label>
+                {{ Form::radio('Hotel_Status[entry_status]', '1', $hotel_status->entry_status == 1, ['id' => 'k2',]) }}
+                {{ Form::label('k2', '登録作業中') }}
 
-                <input type="radio" name="Hotel_Status[entry_status]" value="2" id="k3" {{ $hotel_status->entry_status == 2 ? 'checked' : '' }} >
-                <label for="k3">解約</label>
+                {{ Form::radio('Hotel_Status[entry_status]', '2', $hotel_status->entry_status == 2, ['id' => 'k3',]) }}
+                {{ Form::label('k3', '解約') }}
             </td>
         @endif
         <td>
@@ -120,7 +116,7 @@
     <tr>
         <td bgcolor="#EEFFEE">契約日</td>
         <td>
-            <input type="text" name="Hotel_Status[contract_ymd]" value="{{ strip_tags($hotel_status->contract_ymd) }}" size="20" maxlength="15">
+            {{ Form::text('Hotel_Status[contract_ymd]', strip_tags($hotel_status->contract_ymd), ['size' => '20', 'maxlength' => '15',]) }}
         </td>
         <td>YYYY/MM/DD <small>又は</small> YYYY-MM-DD</td>
     </tr>
@@ -128,7 +124,7 @@
     <tr>
         <td bgcolor="#EEFFEE">公開日</td>
         <td>
-            <input type="text" name="Hotel_Status[open_ymd]" value="{{ strip_tags($hotel_status->open_ymd) }}" size="20" maxlength="15">
+            {{ Form::text('Hotel_Status[open_ymd]', strip_tags($hotel_status->open_ymd), ['size' => '20', 'maxlength' => '15',]) }}
         </td>
         <td>YYYY/MM/DD <small>又は</small> YYYY-MM-DD</td>
     </tr>
@@ -143,6 +139,6 @@
         </tr>
     @endif
 
-    <input type="hidden" name="target_cd" value="{{ strip_tags($target_cd) }}">
-    <input type="hidden" name="target_stock_type" value="{{ strip_tags($target_stock_type) }}">
+    {{ Form::hidden('target_cd', strip_tags($target_cd)) }}
+    {{ Form::hidden('target_stock_type', strip_tags($target_stock_type)) }}
 </table>
