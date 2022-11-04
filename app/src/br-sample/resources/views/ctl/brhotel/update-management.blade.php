d7a6b2a8d94706e8e42a54c4183eafbd0c3c270f
Noda <r.noda@a-force.co.jp>
2022-11-04 10:32:42 +0900

    [update] 結果画面 view indent

--- app/src/br-sample/resources/views/ctl/brhotel/update-management.blade.php
+++ app/src/br-sample/resources/views/ctl/brhotel/update-management.blade.php
@@ -5,30 +5,28 @@
 
 @section('page_blade')
 
-{{-- メッセージ --}}
-@include('ctl.common.message', $messages)
+    {{-- メッセージ --}}
+    @include('ctl.common.message', $messages)
 
-{{-- 施設情報詳細 --}}
-@include('ctl.brhotel._hotel_info', [
-    "hotel" => $views->hotel,
-    "mast_pref" => $views->mast_pref,
-    "mast_city" => $views->mast_city,
-    "mast_ward" => $views->mast_ward
-])
+    {{-- 施設情報詳細 --}}
+    @include('ctl.brhotel._hotel_info', [
+        "hotel" => $views->hotel,
+        "mast_pref" => $views->mast_pref,
+        "mast_city" => $views->mast_city,
+        "mast_ward" => $views->mast_ward
+    ])
 
-<br>
+    <br>
 
-<FORM method="POST" action="{$v->env.source_path}{$v->env.module}/brhotel/show/">
+    <FORM method="POST" action="{$v->env.source_path}{$v->env.module}/brhotel/show/">
+        @include('ctl.brhotel._info_management_form')
+        <INPUT TYPE="submit" VALUE="詳細変更へ">
+    </FORM>
 
-    @include('ctl.brhotel._info_management_form')
+    @include('ctl.brhotel._hotel_top_form', [
+        "target_cd" => $views->target_cd
+    ])
 
-    <INPUT TYPE="submit" VALUE="詳細変更へ">
-</FORM>
-
-@include('ctl.brhotel._hotel_top_form', [
-    "target_cd" => $views->target_cd
-])
-
-<br>
+    <br>
 
 @endsection
