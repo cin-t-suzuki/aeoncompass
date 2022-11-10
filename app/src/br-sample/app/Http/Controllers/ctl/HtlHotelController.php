0ea2582e29e40e489cc55e039501a6e70062ba53
Noda <r.noda@a-force.co.jp>
2022-11-10 12:46:38 +0900

    [add] view を追加

--- app/src/br-sample/app/Http/Controllers/ctl/HtlHotelController.php
+++ app/src/br-sample/app/Http/Controllers/ctl/HtlHotelController.php
@@ -10,6 +10,7 @@ class HtlHotelController extends Controller
     //
     public function show()
     {
+        return view('ctl.htlHotel.show');
         return 'HtlHotelController show method';
     }
 }
