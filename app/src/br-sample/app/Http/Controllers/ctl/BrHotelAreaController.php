d4af7c40cc6278d4df4edcbd84f4cb0e21f4a3fa
Noda <r.noda@a-force.co.jp>
2022-11-16 10:55:16 +0900

    [update] 軽微な修正 (random str の読み込み)

--- app/src/br-sample/app/Http/Controllers/ctl/BrHotelAreaController.php
+++ app/src/br-sample/app/Http/Controllers/ctl/BrHotelAreaController.php
@@ -4,84 +4,20 @@ namespace App\Http\Controllers\ctl;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class BrHotelAreaController extends Controller
 {
     public function index(Request $request)
     {
         $targetCd = $request->input('target_cd');
-        $hotelInfo = $this->dummyHotelInfo($targetCd    );
+        $hotelInfo = $this->dummyHotelInfo($targetCd);
         $hotelAreas = [
-            (object)[
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'hotel_cd' => $targetCd,
-            ],
-            (object)[
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'hotel_cd' => $targetCd,
-            ],
-            (object)[
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'hotel_cd' => $targetCd,
-            ],
-            (object)[
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'hotel_cd' => $targetCd,
-            ],
-            (object)[
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'area_nm_l'     => 'area_nm_l_val' . \Str::random(5),
-                'area_nm_p'     => 'area_nm_p_val' . \Str::random(5),
-                'area_nm_m'     => 'area_nm_m_val' . \Str::random(5),
-                'area_nm_s'     => 'area_nm_s_val' . \Str::random(5),
-                'entry_no'      => 'entry_no_val' . \Str::random(5),
-                'hotel_cd' => $targetCd,
-            ],
+            $this->dummyHotelArea($targetCd),
+            $this->dummyHotelArea($targetCd),
+            $this->dummyHotelArea($targetCd),
+            $this->dummyHotelArea($targetCd),
+            $this->dummyHotelArea($targetCd),
         ];
         return view('ctl.brHotelArea.index', [
             'target_cd' => $targetCd,
@@ -93,7 +29,7 @@ class BrHotelAreaController extends Controller
     public function new(Request $request)
     {
         $targetCd = $request->input('target_cd');
-        $hotelInfo = $this->dummyHotelInfo($targetCd    );
+        $hotelInfo = $this->dummyHotelInfo($targetCd);
 
         return view('ctl.brHotelArea.new', [
             'target_cd'     => $targetCd,
@@ -104,7 +40,7 @@ class BrHotelAreaController extends Controller
     public function edit(Request $request)
     {
         $targetCd = $request->input('target_cd');
-        $hotelInfo = $this->dummyHotelInfo($targetCd    );
+        $hotelInfo = $this->dummyHotelInfo($targetCd);
 
         return view('ctl.brHotelArea.edit', [
             'target_cd'     => $targetCd,
@@ -132,16 +68,28 @@ class BrHotelAreaController extends Controller
     }
 
     // TODO: to be deleted
-    private function dummyHotelInfo($targetCd   )
+    private function dummyHotelInfo($targetCd)
     {
         return (object)[
             'hotel_cd'  => $targetCd,
-            'hotel_nm'  => 'hotel_nm_' . \Str::random(4),
-            'postal_cd' => 'postal_cd_' . \Str::random(4),
-            'pref_nm'   => 'pref_nm_' . \Str::random(4),
-            'address'   => 'address_' . \Str::random(4),
-            'tel'       => 'tel_' . \Str::random(4),
-            'fax'       => 'fax_' . \Str::random(4),
+            'hotel_nm'  => 'hotel_nm_' . Str::random(4),
+            'postal_cd' => 'postal_cd_' . Str::random(4),
+            'pref_nm'   => 'pref_nm_' . Str::random(4),
+            'address'   => 'address_' . Str::random(4),
+            'tel'       => 'tel_' . Str::random(4),
+            'fax'       => 'fax_' . Str::random(4),
+        ];
+    }
+    // TODO: to be deleted
+    private function dummyHotelArea($targetCd)
+    {
+        return (object)[
+            'entry_no'      => 'entry_no_val' . Str::random(5),
+            'area_nm_l'     => 'area_nm_l_val' . Str::random(5),
+            'area_nm_m'     => 'area_nm_m_val' . Str::random(5),
+            'area_nm_p'     => 'area_nm_p_val' . Str::random(5),
+            'area_nm_s'     => 'area_nm_s_val' . Str::random(5),
+            'hotel_cd'      => $targetCd,
         ];
     }
 }
