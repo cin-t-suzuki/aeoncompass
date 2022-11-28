2f3acf9a7e52a2c0e3e76633d0acd45fcae47545
807617 <kikuchi-miki15@aeonpeople.biz>
2022-11-28 13:36:57 +0900

    phpcsエラー対応

--- app/src/br-sample/app/Http/Controllers/ctl/BrBillPayPtnController.php
+++ app/src/br-sample/app/Http/Controllers/ctl/BrBillPayPtnController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\ctl;
 
 use App\Common\DateUtil;
 use App\Http\Controllers\ctl\_commonController;
-
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\DB;
 use Exception;
@@ -14,48 +13,45 @@ use App\Models\BillPayPtn;
 
 class BrBillPayPtnController extends _commonController
 {
+    use Traits;
 
-	use Traits;
+    // パートナー精算書一覧表示
+    public function list()
+    {
 
-	// パートナー精算書一覧表示
-	public function list()
-	{
+        // データを取得
+        $requestBrBillPayPtn = Request::all();
+        $year = $requestBrBillPayPtn['year'] ?? null;
+        $month = $requestBrBillPayPtn['month'] ?? null;
 
-		// データを取得
-		$requestBrBillPayPtn = Request::all();
-		$year = $requestBrBillPayPtn['year']??null;
-		$month = $requestBrBillPayPtn['month']??null;
+        $BillPayPtnModel = new BillPayPtn();
 
-		$BillPayPtnModel = new BillPayPtn();
+        // オブジェクトの取得
+        $o_models_date = new DateUtil();
 
-		// オブジェクトの取得
-		$o_models_date = new DateUtil();
+        if ($this->is_empty($year)) {
+            // $this->_request->setParam('year', $o_models_date->to_format('Y'));
+            $year = $o_models_date->to_format('Y');
+        }
+        if ($this->is_empty($month)) {
+            // $this->_request->setParam('month', $o_models_date->to_format('m'));
+            $month = $o_models_date->to_format('m');
+        }
 
-		if ($this->is_empty($year)){
-			// $this->_request->setParam('year', $o_models_date->to_format('Y'));
-			$year = $o_models_date->to_format('Y');
-		}
-		if ($this->is_empty($month)){
-			// $this->_request->setParam('month', $o_models_date->to_format('m'));
-			$month = $o_models_date->to_format('m');
-		}
+        $o_billpay_ym = new DateUtil($year . '-' . $month . '-01');
+        $billpayptn = $BillPayPtnModel->getBillPayPtn(array('billpay_ym' => $o_billpay_ym->to_format('Y-m')));
 
-		$o_billpay_ym = new DateUtil($year . '-'. $month . '-01');
-		$billpayptn = $BillPayPtnModel->_get_billpayptn(array('billpay_ym' => $o_billpay_ym->to_format('Y-m'))); //ビューにセット？
+        $error = ''; //追記初期化、下記変更
+        if ($error == 'NotFound') {
+            $guide = "精算書が作成されたパートナーはありませんでした。";
+        }
 
-		$error = ''; //追記初期化、下記変更
-		if ($error == 'NotFound') {
-			$guide = "精算書が作成されたパートナーはありませんでした。";
-		}
+        // データを ビューにセット
+        $this->addViewData("billpayptn", $billpayptn);
+        $this->addViewData("year", $year);
+        $this->addViewData("month", $month);
 
-		// データを ビューにセット
-		$this->addViewData("billpayptn", $billpayptn); 
-		$this->addViewData("year", $year); 
-		$this->addViewData("month", $month); 
-
-		// ビューを表示
-		return view("ctl.brbillpayptn.list", $this->getViewData());
-
-	}
-
+        // ビューを表示
+        return view("ctl.brbillpayptn.list", $this->getViewData());
+    }
 }
