045f515751c5213b3edcd419d538e0a9694ea4fe
Noda <r.noda@a-force.co.jp>
2022-12-05 12:43:54 +0900

    [update] 施設状態 入力・登録処理 (5,6 / 6) view formatter 適用

--- app/src/br-sample/resources/views/ctl/brhotel/_info_state_form.blade.php
+++ app/src/br-sample/resources/views/ctl/brhotel/_info_state_form.blade.php
@@ -1,158 +1,177 @@
 {{-- MEMO: 移植元 public\app\ctl\views\brhotel\_info_state_form.tpl --}}
 
 <table border="1" cellspacing="0" cellpadding="3">
+    <tr>
+        <td bgcolor="#EEFFEE">施設コード</td>
+        <td>
+            {{ strip_tags($target_cd) }}<br />
+        </td>
+    </tr>
 
+    <tr>
+        <td bgcolor="#EEFFEE">通知媒体</td>
+        <td>
+            {{-- TODO: $loop 変数を使って書く --}}
+            @php $device_first = true @endphp
+            @foreach ($notify_device as $value)
+                @if ($device_first != true)
+                    +
+                @endif
+                @if ($value == 1)
+                    ファックス
+                    @php $device_first = false @endphp
+                @elseif ($value == 2)
+                    電子メール
+                    @php $device_first = false @endphp
+                @elseif ($value == 4)
+                    オペレータ連絡
+                    @php $device_first = false @endphp
+                @elseif ($value == 8)
+                    リンカーン
+                    @php $device_first = false @endphp
+                @endif
+            @endforeach
+            <br />
+        </td>
+    </tr>
 
     <tr>
-      <td  bgcolor="#EEFFEE" >施設コード</td>
-      <td>
-        {{strip_tags($target_cd)}}<br>
-      </td>
+        <td bgcolor="#EEFFEE">予約情報プッシュ通知（ねっぱん）</td>
+        <td>
+            @if ($hotel_notify->neppan_status === '1')
+                通知する
+            @elseif ($hotel_notify->neppan_status === '0')
+                通知しない
+            @else
+                通知しない(※連動時に「通知する」に自動切替)
+            @endif
+            <br />
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >通知媒体</td>
-      <td>
-        {{-- TODO: $loop 変数を使って書く --}}
-        @php $device_first = true @endphp
-        @foreach ($notify_device as $value)
-          @if ($device_first != true)+@endif
-          @if ($value == 1)ファックス@php $device_first = false @endphp
-          @elseif ($value == 2)電子メール@php $device_first = false @endphp
-          @elseif ($value == 4)オペレータ連絡@php $device_first = false @endphp
-          @elseif ($value == 8)リンカーン@php $device_first = false @endphp
-          @endif
-        @endforeach
-        <br>
-      </td>
+        <td bgcolor="#EEFFEE">通知ステータス</td>
+        <td>
+            @if ($hotel_notify->notify_status == 0)
+                通知しない
+            @elseif ($hotel_notify->notify_status == 1)
+                通知する
+            @endif
+            <br />
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >予約情報プッシュ通知（ねっぱん）</td>
-      <td>
-        @if ($hotel_notify->neppan_status === '1')
-          通知する
-        @elseif ($hotel_notify->neppan_status === '0')
-          通知しない
-        @else 
-          通知しない(※連動時に「通知する」に自動切替)
-        @endif<br>
-      </td>
+        <td bgcolor="#EEFFEE">通知電子メールアドレス</td>
+        <td>
+            {{ strip_tags($hotel_notify->notify_email) }}<br />
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >通知ステータス</td>
-      <td>
-        @if ($hotel_notify->notify_status == 0)
-          通知しない
-        @elseif ($hotel_notify->notify_status == 1)
-          通知する
-        @endif<br>
-      </td>
+        <td bgcolor="#EEFFEE">通知ファックス番号</td>
+        <td>
+            {{ strip_tags($hotel_notify->notify_fax) }}<br />
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >通知電子メールアドレス</td>
-      <td>
-        {{strip_tags($hotel_notify->notify_email)}}<br>
-      </td>
+        <td bgcolor="#EEFFEE">FAXPR</td>
+        <td>
+            @if ($hotel_notify->faxpr_status == 0)
+                表示しない
+            @elseif ($hotel_notify->faxpr_status == 1)
+                表示する
+            @endif
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >通知ファックス番号</td>
-      <td>
-        {{strip_tags($hotel_notify->notify_fax)}}<br>
-      </td>
+        <td bgcolor="#EEFFEE">仕入タイプ</td>
+        <td>
+            @if ($hotel_control->stock_type == 0)
+                受託販売
+            @elseif ($hotel_control->stock_type == 1)
+                買取販売
+            @elseif ($hotel_control->stock_type == 2)
+                一括受託（東横イン）
+            @elseif ($hotel_control->stock_type == 3)
+                特定施設(三普)
+            @endif
+            <br />
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >FAXPR</td>
-      <td>
-        @if ($hotel_notify->faxpr_status == 0)
-          表示しない
-        @elseif ($hotel_notify->faxpr_status == 1)
-          表示する
-        @endif
-      </td>
+        <td bgcolor="#EEFFEE">送客実績送信</td>
+        <td>
+            @if ($hotel_control->checksheet_send == 0)
+                送信しない
+            @elseif ($hotel_control->checksheet_send == 1)
+                送信する
+            @endif
+            <br />
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >仕入タイプ</td>
-      <td>
-        @if ($hotel_control->stock_type == 0)
-          受託販売
-        @elseif ($hotel_control->stock_type == 1)
-          買取販売
-        @elseif ($hotel_control->stock_type == 2)
-          一括受託（東横イン）
-        @elseif ($hotel_control->stock_type == 3)
-          特定施設(三普)
-        @endif<br>
-      </td>
+        <td bgcolor="#EEFFEE">金額切り捨て桁</td>
+        <td>
+            {{-- TODO: is_empty -> is_null に書き換えた。問題ないか確認 --}}
+            @if ($hotel_control->charge_round == 1 || is_null($hotel_control->charge_round))
+                1の位で丸める
+            @elseif ($hotel_control->charge_round == 10)
+                10の位で丸める
+            @elseif ($hotel_control->charge_round == 100)
+                100の位で丸める
+            @endif
+            <br />
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >送客実績送信</td>
-      <td>
-        @if ($hotel_control->checksheet_send == 0)
-          送信しない
-        @elseif ($hotel_control->checksheet_send == 1)
-          送信する
-        @endif<br>
-      </td>
+        <td bgcolor="#EEFFEE">連泊限界数</td>
+        <td>
+            {{ strip_tags($hotel_control->stay_cap) }}<br />
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >金額切り捨て桁</td>
-      <td>
-        {{-- TODO: is_empty -> is_null に書き換えた。問題ないか確認 --}}
-        @if ($hotel_control->charge_round == 1 || is_null($hotel_control->charge_round))
-          1の位で丸める
-        @elseif ($hotel_control->charge_round == 10)
-          10の位で丸める
-        @elseif ($hotel_control->charge_round == 100)
-          100の位で丸める
-        @endif<br>
-      </td>
+        <td bgcolor="#EEFFEE">利用方法</td>
+        <td>
+            @if ($hotel_control->management_status == 1)
+                ファックス管理 <br />
+            @elseif ($hotel_control->management_status == 2)
+                インターネット管理<br />
+            @elseif ($hotel_control->management_status == 3)
+                ファックス管理＋インターネット管理<br />
+            @endif
+        </td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >連泊限界数</td>
-      <td>
-        {{strip_tags($hotel_control->stay_cap)}}<br>
-      </td>
+        <td bgcolor="#EEFFEE">管理システムバージョン※</td>
+        <td>
+            {{-- TODO: magic number --}}
+            @if (in_array(1, $version) and in_array(2, $version))
+                旧インターフェース / 新インターフェース
+            @elseif (in_array(1, $version))
+                旧インターフェース
+            @elseif (in_array(2, $version))
+                新インターフェース
+            @endif
+        </td>
     </tr>
+
     <tr>
-      <td  bgcolor="#EEFFEE" >利用方法</td>
-        @if ($hotel_control->management_status == 1)
-          <td>ファックス管理 <br>
-        @elseif ($hotel_control->management_status == 2)
-          <td>インターネット管理<br>
-        @elseif ($hotel_control->management_status == 3)
-          <td>ファックス管理＋インターネット管理<br>
-        @endif
-      </td>
+        <td bgcolor="#EEFFEE">日本旅行在庫連携</td>
+        <td>
+            @if ($hotel_control->akafu_status == 1)
+                利用する
+            @else
+                利用しない
+            @endif
+        </td>
     </tr>
-    <tr>
-      <td  bgcolor="#EEFFEE" >管理システムバージョン※</td>
-      <td>
-        {{-- TODO: magic number --}}
-        @if (    in_array(1, $version) and in_array(2, $version))
-          旧インターフェース / 新インターフェース
-        @elseif (in_array(1, $version))
-          旧インターフェース
-        @elseif (in_array(2, $version))
-          新インターフェース
-        @endif
-      </td>
-    </tr>
-    <tr>
-      <td  bgcolor="#EEFFEE" >日本旅行在庫連携</td>
-      <td>
-        @if ($hotel_control->akafu_status == 1)利用する
-        @else 利用しない
-        @endif
-      </td>
-    </tr>
-    <input type="hidden" name="target_cd" value="{{strip_tags($target_cd)}}">
-  </table>
+    <input type="hidden" name="target_cd" value="{{ strip_tags($target_cd) }}">
+</table>
--- app/src/br-sample/resources/views/ctl/brhotel/_input_state_form.blade.php
+++ app/src/br-sample/resources/views/ctl/brhotel/_input_state_form.blade.php
@@ -1,197 +1,238 @@
 {{-- MEMO: 移植元 public\app\ctl\views\brhotel\_input_state_form.tpl --}}
 
 <table border="1" cellspacing="0" cellpadding="3">
+    <tr>
+        <td bgcolor="#EEFFEE">施設コード</td>
+        <td>
+            {{ strip_tags($target_cd) }}
+        </td>
+        <td></td>
+    </tr>
 
+    <tr>
+        <td bgcolor="#EEFFEE">通知媒体</td>
+        <td>
+            <input type="checkbox" name="notify_device[]" value="1"
+                @if (count($notify_device) === 0)
+                    checked
+                @else
+                    @foreach ($notify_device as $item)
+                        @if ($item == 1)
+                            checked
+                        @endif
+                    @endforeach
+                @endif
+            id="nd1">
+            <label for="nd1">ファックス</label>
+
+            <input type="checkbox" name="notify_device[]" value="2"
+                @foreach ($notify_device as $item) @if ($item == 2) checked @endif @endforeach
+                id="nd2">
+            <label for="nd2">電子メール</label>
+
+            <input type="checkbox" name="notify_device[]" value="4"
+                @foreach ($notify_device as $item) @if ($item == 4) checked @endif @endforeach
+                id="nd3">
+            <label for="nd3">オペレータ連絡</label>
+
+            <input type="checkbox" name="notify_device[]" value="8"
+                @foreach ($notify_device as $item) @if ($item == 8) checked @endif @endforeach
+                id="nd4">
+            <label for="nd4">リンカーン</label>
+        </td>
+        <td>
+            <small>選択 <font color="#ff0000">※ここを変更する場合は2行下の通知ステータスも必ず確認すること</font></small>
+        </td>
+    </tr>
 
     <tr>
-      <td  bgcolor="#EEFFEE" >施設コード</td>
-      <td>
-        {{strip_tags($target_cd)}}
-      </td>
-      <td><br /></td>
+        <td bgcolor="#EEFFEE">予約情報プッシュ通知（ねっぱん）</td>
+        <td>
+            <label>
+                <input type="radio" name="Hotel_Notify[neppan_status]" value="1"
+                    @if ($hotel_notify->neppan_status === '1') checked="checked" @endif />
+                通知する
+            </label>
+
+            <label>
+                <input type="radio" name="Hotel_Notify[neppan_status]" value="0"
+                    @if ($hotel_notify->neppan_status === '0') checked="checked" @endif />
+                通知しない
+            </label>
+
+            {{-- MEMO: 移植元は is_empty --}}
+            <label>
+                <input type="radio" name="Hotel_Notify[neppan_status]" value=""
+                    @if (is_null($hotel_notify->neppan_status)) checked="checked" @endif />
+                通知しない(※連動時に「通知する」に自動切替)
+            </label>
+        </td>
+        <td><small>選択</small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >通知媒体</td>
-      <td>
-        <input type="checkbox" name="notify_device[]" value="1" @if ({{-- MEMO: 移植元は is_empty --}} is_null($notify_device)) checked @else @foreach ($notify_device as $item) @if ($item == 1) checked @endif @endforeach @endif id="nd1">
-          <label for="nd1">
-            ファックス
-          </label>
-        <input type="checkbox" name="notify_device[]" value="2" @foreach ($notify_device as $item) @if ($item == 2) checked @endif @endforeach id="nd2">
-          <label for="nd2">
-            電子メール
-          </label>
-        <input type="checkbox" name="notify_device[]" value="4" @foreach ($notify_device as $item) @if ($item == 4) checked @endif @endforeach id="nd3">
-          <label for="nd3">
-            オペレータ連絡
-          </label>
-        <input type="checkbox" name="notify_device[]" value="8" @foreach ($notify_device as $item) @if ($item == 8) checked @endif @endforeach id="nd4">
-          <label for="nd4">
-            リンカーン
-          </label>
-      </td>
-      <td><small>選択 <font color="#ff0000">※ここを変更する場合は2行下の通知ステータスも必ず確認すること</font></small></td>
+        <td bgcolor="#EEFFEE">通知ステータス</td>
+        <td>
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="radio" name="Hotel_Notify[notify_status]" value="1"
+                @if ($hotel_notify->notify_status == 1 || is_null($hotel_notify->notify_status)) checked @endif id="j4">
+            <label for="j4">通知する</label>
+
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="radio" name="Hotel_Notify[notify_status]" value="0"
+                @if ($hotel_notify->notify_status == 0 && !is_null($hotel_notify->notify_status)) checked @endif id="j5">
+            <label for="j5">通知しない</label>
+        </td>
+        <td><small>選択<font color="#ff0000">※ここが「通知しない」だとファックス、電子メール、リンカーンの通知はされません</font></small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE">予約情報プッシュ通知（ねっぱん）</td>
-      <td>
-        <label><input type="radio" name="Hotel_Notify[neppan_status]" value="1" @if ($hotel_notify->neppan_status === '1' ) checked="checked" @endif />通知する</label>
-        <label><input type="radio" name="Hotel_Notify[neppan_status]" value="0" @if ($hotel_notify->neppan_status === '0' ) checked="checked" @endif />通知しない</label>
-        <label><input type="radio" name="Hotel_Notify[neppan_status]" value="" @if ({{-- MEMO: 移植元は is_empty --}} is_null($hotel_notify->neppan_status)) checked="checked" @endif />通知しない(※連動時に「通知する」に自動切替)</label>
-      </td>
-      <td><small>選択</small></td>
+        <td bgcolor="#EEFFEE">通知電子メールアドレス</td>
+        <td>
+            <input type="text" name="Hotel_Notify[notify_email]"
+                value="{{ strip_tags($hotel_notify->notify_email) }}" size="50" maxlength="50">
+        </td>
+        <td></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >通知ステータス</td>
-      <td>
-        <input type="radio" name="Hotel_Notify[notify_status]" value="1" @if ($hotel_notify->notify_status == 1 || {{-- MEMO: 移植元は is_empty --}} is_null($hotel_notify->notify_status)) checked @endif id="j4">
-          <label for="j4">
-            通知する
-          </label>
-        <input type="radio" name="Hotel_Notify[notify_status]" value="0" @if ($hotel_notify->notify_status == 0 && !{{-- MEMO: 移植元は is_empty --}} is_null($hotel_notify->notify_status)) checked @endif id="j5">
-          <label for="j5">
-            通知しない
-          </label>
-      </td>
-      <td><small>選択<font color="#ff0000">※ここが「通知しない」だとファックス、電子メール、リンカーンの通知はされません</font></small></td>
+        <td bgcolor="#EEFFEE">通知ファックス番号</td>
+        <td>
+            <input type="text" name="Hotel_Notify[notify_fax]" value="{{ strip_tags($hotel_notify->notify_fax) }}"
+                size="20" maxlength="15">
+        </td>
+        <td><small>xxxx-xxxx-xxxx</small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >通知電子メールアドレス</td>
-      <td>
-        <input type="text" name="Hotel_Notify[notify_email]" value="{{strip_tags($hotel_notify->notify_email)}}" size="50" maxlength="50">
-      </td>
-      <td><br /></td>
+        <td bgcolor="#EEFFEE">FAXPR</td>
+        <td>
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="radio" name="Hotel_Notify[faxpr_status]" value="1"
+                @if ($hotel_notify->faxpr_status == 1 || is_null($hotel_notify->faxpr_status)) checked @endif id="j9">
+            <label for="j9">表示する</label>
+
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="radio" name="Hotel_Notify[faxpr_status]" value="0"
+                @if ($hotel_notify->faxpr_status == 0 && !is_null($hotel_notify->faxpr_status)) checked @endif id="j8">
+            <label for="j8">表示しない</label>
+        </td>
+        <td><small>選択</small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >通知ファックス番号</td>
-      <td>
-        <input type="text" name="Hotel_Notify[notify_fax]" value="{{strip_tags($hotel_notify->notify_fax)}}" size="20" maxlength="15">
-      </td>
-      <td><small>xxxx-xxxx-xxxx</small></td>
+        <td bgcolor="#EEFFEE">仕入タイプ</td>
+        <td>
+            @if ($hotel_control->stock_type == '2') 一括受託（東横イン）
+                <input type="hidden" name="Hotel_Control[stock_type]" value="2" />
+            @else
+                <input type="radio" name="Hotel_Control[stock_type]" value="0"
+                    @if ($hotel_control->stock_type == 0) checked @endif id="i1">
+                <label for="i1">受託販売</label>
+
+                <input type="radio" name="Hotel_Control[stock_type]" value="1"
+                    @if ($hotel_control->stock_type == 1) checked @endif id="i2">
+                <label for="i2">買取販売</label>
+
+                <input type="radio" name="Hotel_Control[stock_type]" value="3"
+                    @if ($hotel_control->stock_type == 3) checked @endif id="i3">
+                <label for="i3">特定施設(三普)</label>
+            @endif
+        </td>
+        <td><small>選択</small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >FAXPR</td>
-      <td>
-        <input type="radio" name="Hotel_Notify[faxpr_status]" value="1" @if ($hotel_notify->faxpr_status == 1 || {{-- MEMO: 移植元は is_empty --}} is_null($hotel_notify->faxpr_status)) checked @endif id="j9">
-          <label for="j9">
-            表示する
-          </label>
-        <input type="radio" name="Hotel_Notify[faxpr_status]" value="0" @if ($hotel_notify->faxpr_status == 0 && !{{-- MEMO: 移植元は is_empty --}} is_null($hotel_notify->faxpr_status)) checked @endif id="j8">
-          <label for="j8">
-            表示しない
-          </label>
-      </td>
-      <td><small>選択</small></td>
+        <td bgcolor="#EEFFEE">送客実績送信</td>
+        <td>
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="radio" name="Hotel_Control[checksheet_send]" value="1"
+                @if ($hotel_control->checksheet_send == 1 && !is_null($hotel_control->checksheet_send)) checked @endif id="i4">
+            <label for="i4">送信する</label>
+
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="radio" name="Hotel_Control[checksheet_send]" value="0"
+                @if ($hotel_control->checksheet_send == 0 || is_null($hotel_control->checksheet_send)) checked @endif id="i3">
+            <label for="i3">送信しない</label>
+        </td>
+        <td><small>選択</small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >仕入タイプ</td>
-      <td>
-  @if ($hotel_control->stock_type == "2") 一括受託（東横イン）<input type="hidden" name="Hotel_Control[stock_type]" value="2" />
-  @else
-        <label>
-        <input type="radio" name="Hotel_Control[stock_type]" value="0" @if ($hotel_control->stock_type == 0) checked @endif id="i1">
-          <label for="i1">
-            受託販売
-          </label>
-        </label>
-        <label>
-        <input type="radio" name="Hotel_Control[stock_type]" value="1" @if ($hotel_control->stock_type == 1) checked @endif id="i2">
-          <label for="i2">
-            買取販売
-          </label>
-      <input type="radio" name="Hotel_Control[stock_type]" value="3" @if ($hotel_control->stock_type == 3) checked @endif id="i3">
-          <label for="i3">
-            特定施設(三普)
-          </label>
-   @endif 
-      </td>
-      <td><small>選択</small></td>
+        <td bgcolor="#EEFFEE">金額切り捨て桁</td>
+        <td>
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="radio" name="Hotel_Control[charge_round]" value="1"
+                @if ($hotel_control->charge_round == 1 || is_null($hotel_status->entry_status)) checked @endif id="i5">
+            <label for="i5">1の位で丸める</label>
+
+            <input type="radio" name="Hotel_Control[charge_round]" value="10"
+                @if ($hotel_control->charge_round == 10) checked @endif id="i6">
+            <label for="i6">10の位で丸める</label>
+
+            <input type="radio" name="Hotel_Control[charge_round]" value="100"
+                @if ($hotel_control->charge_round == 100) checked @endif id="i7">
+            <label for="i7">100の位で丸める</label>
+        </td>
+        <td><small>選択</small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >送客実績送信</td>
-      <td>
-        <input type="radio" name="Hotel_Control[checksheet_send]" value="1" @if ($hotel_control->checksheet_send == 1 && !{{-- MEMO: 移植元は is_empty --}} is_null($hotel_control->checksheet_send)) checked @endif id="i4">
-          <label for="i4">
-            送信する
-          </label>
-        <input type="radio" name="Hotel_Control[checksheet_send]" value="0" @if ($hotel_control->checksheet_send == 0 || {{-- MEMO: 移植元は is_empty --}} is_null($hotel_control->checksheet_send)) checked @endif id="i3">
-          <label for="i3">
-            送信しない
-          </label>
-      </td>
-      <td><small>選択</small></td>
+        <td bgcolor="#EEFFEE">連泊限界数</td>
+        <td>
+            <input type="text" name="Hotel_Control[stay_cap]" value="{{ strip_tags($hotel_control->stay_cap) }}"
+                size="4" maxlength="2"><small>設定する場合入力</small>
+        </td>
+        <td><small>数字2桁</small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >金額切り捨て桁</td>
-      <td>
-        <label>
-        <input type="radio" name="Hotel_Control[charge_round]" value="1" @if ($hotel_control->charge_round == 1 || {{-- MEMO: 移植元は is_empty --}} is_null($hotel_status->entry_status)) checked @endif id="i5">
-          <label for="i5">
-            1の位で丸める
-          </label>
-          </label>
-        <label>
-        <input type="radio" name="Hotel_Control[charge_round]" value="10" @if ($hotel_control->charge_round == 10) checked @endif id="i6">
-          <label for="i6">
-            10の位で丸める
-          </label>
-        <input type="radio" name="Hotel_Control[charge_round]" value="100" @if ($hotel_control->charge_round == 100) checked @endif id="i7">
-          <label for="i7">
-            100の位で丸める
-          </label>
-      </td>
-      <td><small>選択</small></td>
+        <td bgcolor="#EEFFEE">利用方法</td>
+        <td>
+            <input type="radio" name="Hotel_Control[management_status]" value="1"
+                @if ($hotel_control->management_status == 1) checked @endif id="management_status1">
+            <label for="management_status1">ファックス管理</label>
+
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="radio" name="Hotel_Control[management_status]" value="2"
+                @if ($hotel_control->management_status == 2 || is_null($hotel_control->management_status)) checked @endif id="management_status2">
+            <label for="management_status2">インターネット管理</label>
+
+            <input type="radio" name="Hotel_Control[management_status]" value="3"
+                @if ($hotel_control->management_status == 3) checked @endif id="management_status3">
+            <label for="management_status3">ファックス管理＋インターネット管理</label>
+        </td>
+        <td><small>選択</small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >連泊限界数</td>
-      <td>
-        <input type="text" name="Hotel_Control[stay_cap]" value="{{strip_tags($hotel_control->stay_cap)}}" size="4" maxlength="2"><small>設定する場合入力</small>
-      </td>
-      <td><small>数字2桁</small></td>
+        <td bgcolor="#EEFFEE">管理システムバージョン</td>
+        <td>
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="checkbox" name="version[]" value="1" id="system_version1"
+                @if (!is_null($version) and in_array(1, $version)) checked @endif />
+            <label for="system_version1">旧インターフェース</label>
+
+            {{-- MEMO: 移植元は is_empty --}}
+            <input type="checkbox" name="version[]" value="2" id="system_version2"
+                @if (is_null($version) or in_array(2, $version)) checked @endif />
+            <label for="system_version2">新インターフェース</label>
+        </td>
+        <td><small>複数選択可<font color="#0000ff">（必須）</font></small></td>
     </tr>
-  
+
     <tr>
-      <td  bgcolor="#EEFFEE" >利用方法</td>
-      <td>
-        <label>
-        <input type="radio" name="Hotel_Control[management_status]" value="1" @if ($hotel_control->management_status == 1) checked @endif id="management_status1">
-          <label for="management_status1">ファックス管理</label>
-        </label>
-        <label>
-        <input type="radio" name="Hotel_Control[management_status]" value="2" @if ($hotel_control->management_status == 2 || {{-- MEMO: 移植元は is_empty --}} is_null($hotel_control->management_status)) checked @endif id="management_status2">
-          <label for="management_status2">インターネット管理</label>
-        <label>
-        <input type="radio" name="Hotel_Control[management_status]" value="3" @if ($hotel_control->management_status == 3) checked @endif id="management_status3">
-          <label for="management_status3">ファックス管理＋インターネット管理</label>
-      </td>
-      <td><small>選択</small></td>
+        <td bgcolor="#EEFFEE">日本旅行在庫連携</td>
+        <td>
+            <input type="radio" id="akafu_status_1" name="Hotel_Control[akafu_status]" value="1"
+                @if ($hotel_control->akafu_status == 1) checked="checked" @endif />
+            <label for="akafu_status_1">利用する</label>
+
+            <input type="radio" id="akafu_status_0" name="Hotel_Control[akafu_status]" value="0"
+                @if ($hotel_control->akafu_status != 1) checked="checked" @endif />
+            <label for="akafu_status_0">利用しない</label>
+        </td>
+        <td><small>選択</small></td>
     </tr>
-    <tr>
-      <td  bgcolor="#EEFFEE" >管理システムバージョン</td>
-      <td>
-        <input type="checkbox" name="version[]" value="1" id="system_version1" @if (!{{-- MEMO: 移植元は is_empty --}} is_null($version) and in_array(1, $version)) checked @endif /><label for="system_version1">旧インターフェース</label>
-        <input type="checkbox" name="version[]" value="2" id="system_version2" @if ({{-- MEMO: 移植元は is_empty --}} is_null($version) or in_array(2, $version)) checked @endif /><label for="system_version2">新インターフェース</label>
-      </td>
-      <td><small>複数選択可<font color="#0000ff">（必須）</font></small></td>
-    </tr>
-    <tr>
-      <td  bgcolor="#EEFFEE" >日本旅行在庫連携</td>
-      <td>
-        <input type="radio" id="akafu_status_1" name="Hotel_Control[akafu_status]" value="1" @if ($hotel_control->akafu_status == 1) checked="checked" @endif /><label for="akafu_status_1">利用する</label>
-        <input type="radio" id="akafu_status_0" name="Hotel_Control[akafu_status]" value="0" @if ($hotel_control->akafu_status != 1) checked="checked" @endif /><label for="akafu_status_0">利用しない</label>
-      </td>
-      <td><small>選択</small></td>
-    </tr>
-    <input type="hidden" name="target_cd" value="{{strip_tags($target_cd)}}">
-  </table>
-  
+    <input type="hidden" name="target_cd" value="{{ strip_tags($target_cd) }}">
+</table>
