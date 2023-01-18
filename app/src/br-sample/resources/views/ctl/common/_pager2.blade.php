c91c1a20cfb1ffe62c5c48e7e1528484a9ae7d9a
807617 <kikuchi-miki15@aeonpeople.biz>
2023-01-18 17:45:47 +0900

    ページャー部分修正

--- app/src/br-sample/resources/views/ctl/common/_pager2.blade.php
+++ app/src/br-sample/resources/views/ctl/common/_pager2.blade.php
@@ -1,71 +1,81 @@
-{* 引数：$pager     ※ページャーオブジェクト             *}
-{* 引数：$params    ※リンクパラメータ                   *}
-{* 引数：$page_name ※ページ番号パラメータの名称         *}
-{*------------------------------------------------------------------------------*}
-{* ページ番号パラメータの名称設定 *}
-{if is_empty($page_name)}
-  {assign var=page_nm value='page'}
-{else}
-  {assign var=page_nm value=$page_name}
-{/if}
-{* /ページ番号パラメータの名称設定 *}
-{* Getパラメータ作成 *}
-{assign var=get_params value=''}
-{foreach from=$params item=value key=key}
+{{-- 引数：$pager     ※ページャーオブジェクト             --}}
+{{-- 引数：$params    ※リンクパラメータ                   --}}
+{{-- 引数：$page_name ※ページ番号パラメータの名称         --}}
+{{----------------------------------------------------------------------------------}}
+{{-- ページ番号パラメータの名称設定 --}}
+@if ($service->is_empty($page_name ?? null))
+  @php $page_nm = 'page' @endphp
+@else
+  @php $page_nm = $page_name @endphp
+@endif
+{{-- /ページ番号パラメータの名称設定 --}}
+{{-- Getパラメータ作成 --}}
+@php $get_params = '' @endphp
+{{-- {foreach from=$params item=value key=key}
   {assign var=get_params value=$get_params|cat:'&amp;'|cat:$key|cat:'='|cat:$value}
-{/foreach}
-{* /Getパラメータ作成 *}
-{*------------------------------------------------------------------------------*}
-{* ページャー *}
-{if $pager->pageCount > 1}
+{/foreach} --}}
+@foreach ($params as $key => $value)
+  @php $get_params = $get_params . '&' . $key . '=' . $value @endphp
+@endforeach
+{{-- /Getパラメータ作成 --}}
+{{----------------------------------------------------------------------------------}}
+{{-- ページャー --}}
+@if ($pager->lastPage() > 1)
   <ul class="pager">
     <li>
-      {* 最初のページへのリンク *}
-      {if !is_empty($pager->previous)}
-        <a href="{$v->env.source_path}{$v->env.module}/{$v->env.controller}/{$v->env.action}/?{$page_nm}={$pager->first}{$get_params}">&lt;&lt;</a>
-      {else}
+      {{-- 最初のページへのリンク --}}
+      @if (!$service->is_empty($pager->previousPageUrl()))
+        <a href="{{$pager->url(1)}}{{$get_params}}">&lt;&lt;</a>
+      @else
         &lt;&lt;
-      {/if}
-      {* /最初のページへのリンク *}
+      @endif
+      {{-- /最初のページへのリンク --}}
     </li>
     <li>
-      {* 前のページへのリンク *}
-      {if !is_empty($pager->previous)}
-        <a href="{$v->env.source_path}{$v->env.module}/{$v->env.controller}/{$v->env.action}/?{$page_nm}={$pager->previous}{$get_params}">PREV</a>
-      {else}
+      {{-- 前のページへのリンク --}}
+      @if (!$service->is_empty($pager->previousPageUrl()))
+        <a href="{{$pager->previousPageUrl()}}{{$get_params}}">PREV</a>
+      @else
         PREV
-      {/if}
-      {* /前のページへのリンク *}
+      @endif
+      {{-- /前のページへのリンク --}}
     </li>
-    {* ページ番号へのリンク *}
-    {foreach from=$pager->pagesInRange item=page}
-      <li {if $page == $pager->current}id="current"{/if}>
-        {if $page == $pager->current}
-          {$page}
-        {else}
-          <a href="{$v->env.source_path}{$v->env.module}/{$v->env.controller}/{$v->env.action}/?{$page_nm}={$page}{$get_params}">{$page}</a>
-        {/if}
+    {{-- ページ番号へのリンク --}}
+    {{--書き換え以下でいいか？ @foreach ($pager->pagesInRange as $page) --}}
+    @php 
+     $start_page = $pager->currentPage() -4;
+      if ($start_page < 1) {$start_page = 1;};
+     $last_page = $pager->currentPage() +5;
+      if ($last_page > $pager->lastPage()) {$last_page = $pager->lastPage();};
+    @endphp
+    @for ($page =$start_page; $page <= $last_page; $page++)
+      <li @if ($page == $pager->currentPage())id="current"@endif>
+        @if ($page == $pager->currentPage())
+          {{$page}}
+        @else
+          <a href="?{{$page_nm}}={{$page}}{{$get_params}}">{{$page}}</a>
+        @endif
       </li>
-    {/foreach}
-    {* /ページ番号へのリンク *}
+    @endfor
+    {{-- /ページ番号へのリンク --}}
     <li>
-      {* 次のページへのリンク *}
-      {if !is_empty($pager->next)}
-        <a href="{$v->env.source_path}{$v->env.module}/{$v->env.controller}/{$v->env.action}/?{$page_nm}={$pager->next}{$get_params}">NEXT</a>
-      {else}
+      {{-- 次のページへのリンク --}}
+      @if (!$service->is_empty($pager->nextPageUrl()))
+        <a href="{{$pager->nextPageUrl()}}{{$get_params}}">NEXT</a>
+      @else
         NEXT
-      {/if}
-      {* /次のページへのリンク *}
+      @endif
+      {{-- /次のページへのリンク --}}
     </li>
     <li>
-      {* 最後のページへのリンク *}
-      {if !is_empty($pager->next)}
-        <a href="{$v->env.source_path}{$v->env.module}/{$v->env.controller}/{$v->env.action}/?{$page_nm}={$pager->last}{$get_params}">&gt;&gt;</a>
-      {else}
+      {{-- 最後のページへのリンク --}}
+      @if (!$service->is_empty($pager->nextPageUrl()))
+        <a href="{{$pager->url($pager->lastPage())}}{{$get_params}}">&gt;&gt;</a>
+      @else
         &gt;&gt;
-      {/if}
-      {* /最後のページへのリンク *}
+      @endif
+      {{-- /最後のページへのリンク --}}
     </li>
   </ul>
-{/if}
-{* /ページャー *}
+@endif
+{{-- /ページャー --}}
