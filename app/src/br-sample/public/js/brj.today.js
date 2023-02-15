df1475b37e19bc4356f4e2e7588a392866d39892
Noda <r.noda@a-force.co.jp>
2023-02-15 14:57:01 +0900

    表示日付を修正

--- app/src/br-sample/public/js/brj.today.js
+++ app/src/br-sample/public/js/brj.today.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
 var w = new Array("日", "月", "火", "水", "木", "金", "土");
-var d = new Date(2022,04-1,21);
+var d = new Date();
 $(".today").text(d.getFullYear() + "年"+ (d.getMonth() + 1) + "月" + d.getDate() + "日（" + w[d.getDay()] + "）");
 });
